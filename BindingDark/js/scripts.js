9113f9acfa14447ab8c3060e8d9a3cc6b4129278
Juan pablo <>
2018-08-25 17:23:29 -0500

    Se arregla binding

--- BindingDark/js/scripts.js
+++ BindingDark/js/scripts.js
@@ -1,31 +1,32 @@
-	window.addEventListener("load",function(event){	
-		searchDocumentVariable();
-	},true);
-	
-	window.addEventListener("click",function(event){
-		searchDocumentVariable();
-	},false);
-	
-	let searchDocumentVariable=function(){
-		for (let item of document.body.children){
-			let getAtt=item.getAttribute("bind");
-			let txtElement =getAtt!=null?getAtt:item.textContent;
-			if(txtElement.search(/[\{\}]+/gm)>=0){	
-				let att=document.createAttribute("bind");
-				att.value=txtElement;
-				item.setAttributeNode(att);
-				let searchVarBind = txtElement.match(/\{+[a-zA-Z\,]+\}+/gm);
-				let getText = searchVarBind!=null?searchVarBind[0].match(/[a-zA-Z]+/gm):"";
-				let concatVar ="";
-				let textToElement = "";
-				for (let nameVar of getText){
-					concatVar+= eval("eval(nameVar)");
-				}
+window.addEventListener("load", function (event) {
+	searchDocumentVariable();
+}, true);
 
-				for (let elementToReplace of searchVarBind){
-					textToElement += txtElement.replace(elementToReplace,concatVar);
+window.addEventListener("click", function (event) {
+	searchDocumentVariable();
+}, false);
+
+let searchDocumentVariable = function () {
+	for (let item of document.body.children) {
+		let getAtt = item.getAttribute("bind");
+		let txtElement = getAtt != null ? getAtt : item.textContent;
+		if (txtElement.search(/[\{\}]+/gm) >= 0) {
+			let att = document.createAttribute("bind");
+			att.value = txtElement;
+			item.setAttributeNode(att);
+			let searchVarBind = txtElement.match(/\{+[a-zA-Z\,]+\}+/gm);
+			let textToElement = txtElement;
+			if(searchVarBind != null){
+				for (const varBind of searchVarBind) {
+					let concatVar = "";
+					let getText = searchVarBind != null ? varBind.match(/[a-zA-Z]+/gm) : "";
+					for (let nameVar of getText) {
+						concatVar += eval("eval(nameVar)");
+					}
+					textToElement = textToElement.replace(varBind, concatVar);
 				}
-				item.textContent= textToElement;
 			}
+			item.textContent = textToElement;
 		}
-	};
+	}
+};
