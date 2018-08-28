e1c0809fd10e2f9dcb485f62b4ec3094ff225593
danslans <danslans77@gmail.com>
2018-08-28 09:29:06 -0500

    Tests

--- BindingDark/js/scripts.js
+++ BindingDark/js/scripts.js
@@ -9,25 +9,31 @@ window.addEventListener("click", function (event) {
 let searchDocumentVariable = function () {
 	for (let item of document.body.children) {
 		let getAtt = item.getAttribute("bind");
-		let txtElement = getAtt != null ? getAtt : item.textContent;
+		let txtElement = getAtt != null ? getAtt : item.textContent!=""?item.textContent:item.value ;
+		//alert(txtElement );
+		
 		if (txtElement.search(/[\{\}]+/gm) >= 0) {
 			let att = document.createAttribute("bind");
 			att.value = txtElement;
 			item.setAttributeNode(att);
-			let searchVarBind = txtElement.match(/\{+[a-zA-Z\,]+\}+/gm);
+			let searchVarBind = txtElement.match(/\{+[a-zA-Z\,\+\*\-\=\(\)]+\}+/gm);
 			let textToElement = txtElement;
 			if(searchVarBind != null){
 				for (const varBind of searchVarBind) {
 					let concatVar = "";
-					let getText = searchVarBind != null ? varBind.match(/[a-zA-Z\+\-]+/gm) : "";
+					let getText = searchVarBind != null ? varBind.match(/[a-zA-Z\+\-\*\=\(\)]+/gm) : "";
 					for (let nameVar of getText) {
-						alert(nameVar);
 						concatVar += eval("eval(nameVar)");
 					}
 					textToElement = textToElement.replace(varBind, concatVar);
 				}
 			}
-			item.textContent = textToElement;
+			//item.textContent = textToElement;
+			if(item.textContent!=""){
+				item.textContent=textToElement;
+			}else{
+			 item.value=textToElement; 
+			 }
 		}
 	}
 };
