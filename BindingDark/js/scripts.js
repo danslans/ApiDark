087e20b0d18fa578a0e9683e243b64361526be30
danslans <danslans77@gmail.com>
2018-09-03 19:00:22 -0500

    modifica estructura

--- BindingDark/js/scripts.js
+++ BindingDark/js/scripts.js
@@ -1,30 +1,21 @@
 window.addEventListener("load", function (event) {
-	bind();
+	bind({});
 }, true);
 
-this.bind=function () {
-	var loopElements = function(element){
-		if(element.children.length>0){
-			for (let item of element.children){
-			alert(item);
-			}
-		}
-	};
-	
-	for (let item of document.body.children) {
-		loopElements(item);
-		/*let getAtt = item.getAttribute("bind");
+this.bind=function (data) {
+	var initBind = function(item){
+		let getAtt = item.getAttribute("bind");
 		let txtElement = getAtt != null ? getAtt : item.textContent!=""?item.textContent:item.value ;
 		if (txtElement.search(/[\{\}]+/gm) >= 0) {
 			let att = document.createAttribute("bind");
 			att.value = txtElement;
 			item.setAttributeNode(att);
-			let searchVarBind = txtElement.match(/\{+[a-zA-Z\,\+\*\-\=\(\)\"]+\}+/gm);
+			let searchVarBind = txtElement.match(/\{+[a-zA-Z\,\+\*\-\=\(\)\"\/1-9]+\}+/gm);
 			let textToElement = txtElement;
 			if(searchVarBind != null){
 				for (const varBind of searchVarBind) {
 					let concatVar = "";
-					let getText = searchVarBind != null ? varBind.match(/[a-zA-Z\+\-\*\=\(\)\"]+/gm) : "";
+					let getText = searchVarBind != null ? varBind.match(/[a-zA-Z\+\-\*\=\(\)\"\/1-9]+/gm) : "";
 					for (let nameVar of getText) {
 						concatVar += eval("eval(nameVar)");
 					}
@@ -38,9 +29,29 @@ this.bind=function () {
 			}else{
 			 item.value=textToElement; 
 			 }
-		}*/
+		}
+	};
+	
+	var loopElements = function(element){
+		if(element.children.length>0){
+			initBind(element);
+			for (let item of element.children){
+			initBind(item);
+			loopElements(item);
+			}
+		}else{
+			initBind(element);
+		}
+	};
+	
+	for (let item of document.body.children) {
+		loopElements(item);	
 	}
+	
+	
 };
+
+
 
 
 
